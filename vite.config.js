d147299e9d88fd27b1e105d1b50c954a497e0ec3
«acaballee» <alexcaballe@iesebre.com>
2025-11-06 19:29:35 +0100

    Equipament Deport i nutricio (Actions arreglat)

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 export default defineConfig({
-build: {
-rollupOptions: {
-input: {
-main: resolve(__dirname, 'index.html'),
-nutricio: resolve(__dirname, 'Nutricio.html'),
-oferts: resolve(__dirname, 'Ofetes.html'),
-deport: resolve(__dirname, 'Deport.html'),
-equipament: resolve(__dirname, 'Equipament.html'),
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nutricio: resolve(__dirname, 'Nutricio.html'),
+                oferts: resolve(__dirname, 'Ofertes.html'),
+                deport: resolve(__dirname, 'Deport.html'),
+                equipament: resolve(__dirname, 'Equipament.html'),
 
-},
-},
-},
+            },
+        },
+    },
 })
